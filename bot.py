c1524d27a02b32364274e6fae7a9c344fa3dec4f
P-skylos <126717281+P-skylos@users.noreply.github.com>
2025-09-23 13:49:48 -0700

    switched to a testing function

--- bot.py
+++ bot.py
@@ -2,9 +2,14 @@ import meshtastic
 import meshtastic.serial_interface
 from pubsub import pub #this is the event/message bus system we snoop on
 
-usb_connection = meshtastic.serial_interface.SerialInterface() #automatically tries to detect device and connect
+# usb_connection = meshtastic.serial_interface.SerialInterface() #automatically tries to detect device and connect
 
-def onReceive(packet): 
+def onReceiveText(packet, interface): 
     print(packet)
 
-pub.subscribe(onReceive, "meshtastic.receive.text") #tell the bus to use our callback whenever a packet is received
+def onReceivePacket(packet, interface): #for testing
+    print(packet)
+
+# pub.subscribe(onReceiveText, "meshtastic.receive.text") #tell the bus to use our callback whenever a packet is received
+
+pub.subscribe(onReceivePacket, "meshtastic.receive")
