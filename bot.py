fe04d4c71c99d3eb55af1395870b04e621743adc
P-skylos <126717281+P-skylos@users.noreply.github.com>
2025-09-23 15:00:10 -0700

    updated bot script for testing

--- bot.py
+++ bot.py
@@ -2,14 +2,17 @@ import meshtastic
 import meshtastic.serial_interface
 from pubsub import pub #this is the event/message bus system we snoop on
 
-# usb_connection = meshtastic.serial_interface.SerialInterface() #automatically tries to detect device and connect
+# even though it doesn't look like it, you need to make this connection, the pubsub messages are a side effect of the interface
+usb_connection = meshtastic.serial_interface.SerialInterface() #automatically tries to detect device and connect
 
 def onReceiveText(packet, interface): 
     print(packet)
 
 def onReceivePacket(packet, interface): #for testing
+    #see example telemetry packet
     print(packet)
 
 # pub.subscribe(onReceiveText, "meshtastic.receive.text") #tell the bus to use our callback whenever a packet is received
 
 pub.subscribe(onReceivePacket, "meshtastic.receive")
+#need to make the application stay on
