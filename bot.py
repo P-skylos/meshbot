e62a0b20673f03e050ed6cdbd05abcf20f6142de
P-skylos <126717281+P-skylos@users.noreply.github.com>
2025-09-23 12:47:28 -0700

    boilerplate listening code

--- bot.py
+++ bot.py
@@ -1,3 +1,10 @@
 import meshtastic
-import serial
+import meshtastic.serial_interface
+from pubsub import pub #this is the event/message bus system we snoop on
 
+usb_connection = meshtastic.serial_interface.SerialInterface() #automatically tries to detect device and connect
+
+def onReceive(packet): 
+    print(packet)
+
+pub.subscribe(onReceive, "meshtastic.receive.text") #tell the bus to use our callback whenever a packet is received
