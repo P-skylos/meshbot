d45d6ba5463fe0dcd8af4903ead79941aa27f8b8
P-skylos <126717281+P-skylos@users.noreply.github.com>
2025-09-24 16:22:19 -0700

    bot updated to use parsing

--- bot.py
+++ bot.py
@@ -2,6 +2,8 @@ import meshtastic
 import meshtastic.serial_interface
 from pubsub import pub #this is the event/message bus system we snoop on
 
+from command_parse import try_text
+
 # even though it doesn't look like it, you need to make this connection, 
 #the pubsub messages are a side effect of the interface
 
@@ -9,7 +11,9 @@ usb_connection = meshtastic.serial_interface.SerialInterface() #automatically tr
 
 def onReceiveText(packet, interface): 
     text = packet["decoded"]["text"]
-    interface.sendText(f"echo: {text}")
+    response = try_text(text)
+    if response is not None:
+        interface.sendText(response)
 
 
 pub.subscribe(onReceiveText, "meshtastic.receive.text") #tell the bus to use our callback whenever a packet is received
